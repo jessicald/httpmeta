5006793dbeb1c4dda5f90a37ee0ed0d77c69a4bb
Joseph Leeper <cordella@gmx.com>
2012-06-05 16:20:34 -0500

    GPL licensing.

--- httpmeta.py
+++ httpmeta.py
@@ -1,4 +1,21 @@
 # -*- coding: utf-8 -*-
+#
+# httpmeta : Lightweight library for fetching HTTP resource metadata.
+# Copyright (C) 2012  Joseph Leeper
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import urllib.parse
 import re
 from random import choice
